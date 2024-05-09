841637885fb1c95f005a8195f848e4cd21e00ba7
vaisberger <yaelw123@gmail.com>
2024-05-09 19:53:42 +0300

    9/5 start

---
+++ AirportUtil.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class AirportUtil {
+    public static void sortTransport(Comparable[] transport){
+        // TODO: implement;
+    }
+    static String reportAll (Movable[] movables) {
+        ArrayList<String> buffer = new ArrayList<>();
+        for (Movable m: movables) {
+            // TODO: implement;
+        }
+        return String.join("\n",buffer);
+    }
+}
---
+++ Plane.java
@@ -0,0 +1,54 @@
+public class Plane implements Movable, Comparable{
+    public static final int HEIGHT_LIMIT = 1500;
+    int licenceNumber;
+    Location source, destination;
+    int maximalHeight;
+
+    public Plane(int id, Location src, Location dst, int maxHeight) {
+        licenceNumber = id;
+        source = src;
+        destination = dst;
+        maximalHeight = maxHeight;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("licence = %d, source = %s, destination = %s, maxHeight = %d",
+                licenceNumber, source, destination, maximalHeight);
+    }
+
+    @Override
+    public String getType() {
+        return "Plane";
+    }
+
+    @Override
+    public int getId() {
+        return licenceNumber;
+    }
+
+    @Override
+    public Location getSource() {
+        return source;
+    }
+
+    @Override
+    public Location getDestination() {
+        return destination;
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return String.format();
+    }
+
+    @Override
+    public void move() {
+        // TODO: implement;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        // TODO: implement;
+        return 0;
+    }
